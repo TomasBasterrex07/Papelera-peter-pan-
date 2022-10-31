2822200a38e042c321230e6ef97b302532cb15c7
tomas <Basterrexcuervo@gmail.com>
2022-10-31 20:44:25 -0300

    agregando js

---
+++ script.js
@@ -0,0 +1,9 @@
+function saludo(){
+    let nombrepersona = prompt("Indique su nombre")
+    let apellidopersona = prompt("Indique su apellido")
+
+    alert(`Bienvenido ${nombrepersona} ${apellidopersona} espero estes teniendo un buen dia`)
+} 
+
+alert("Bienvenido a la papelera Peter Pan")
+saludo()
