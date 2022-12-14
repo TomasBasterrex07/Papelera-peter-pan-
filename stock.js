a06473c234a764c2f1f87b26803b0acbe0ef56cd
tomas <Basterrexcuervo@gmail.com>
2022-12-14 16:06:28 -0300

    agregando script

---
+++ stock.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+    {id: 1, nombre: "Rollo de arranque DRAG 50X70 X 1,2 KG", tipo: "rollo", cantidad: 1, desc: "Calidad premium", precio: 1200, talle: "Grande", img: '../imagenes/Rollo_de_arranque_DRAG_50X70_X_1_2_KG-removebg-preview.png'},
+    {id: 2, nombre: "Rollo de arranque superfino 20X30", tipo: "rollo", cantidad: 1, desc: "Calidad premium", precio: 1100, talle: "Mediano", img: '../imagenes/rollodearranquesuperfino20x30-removebg-preview.png'},
+    {id: 3, nombre: "Rollo de arranque superfino 25X35 ", tipo: "rollo", cantidad: 1, desc: "Calidad premium", precio: 900, talle: "Chico", img: '../imagenes/Rollo_de_arranque_DRAG_50X70_X_1_2_KG-removebg-preview.png'},
+    {id: 4, nombre: "Rollo de arranque superfino 40X50", tipo: "rollo", cantidad: 1, desc: "Calidad premium", precio: 1300, talle: "Extra grande", img: '../imagenes/rollodearranquesuperfino20x30-removebg-preview.png'},
+    {id: 5, nombre: "Bulto X10 camisetas ciudad verde/negra 45X55", tipo: "camisetas", cantidad: 1, desc: "Calidad premium", precio: 500, talle: "chico", img: '../imagenes/BOLSA1SCRIPT.jpg'},
+    {id: 6, nombre: "Bulto X20 camisetas eco blanca 50X50", tipo: "camisetas", cantidad: 1, desc: "Calidad premium", precio: 600, talle: "Mediana", img: '../imagenes/BOLSASBLANCASSCRI.jpg'},
+    {id: 7, nombre: "Paquete X80 camisetas 40X50 reforzadas", tipo: "bulto", cantidad: 1, desc: "Calidad premium", precio: 800, talle: "grande", img: '../imagenes/SUPERREFORZADAS.jpg'},
+    {id: 8, nombre: "Paquete X100 bandeja de carton N°2", tipo: "Bandeja", cantidad: 1, desc: "Calidad premium", precio: 500, talle: "Chico", img: '../imagenes/cajcarton2.png'},
+    {id: 9, nombre: "Paquete X100 bandeja de carton N°3", tipo: "Bandeja", cantidad: 1, desc: "Calidad premium", precio: 1000, talle: "Chico", img: '../imagenes/cajacarton3.png'},
+    {id: 10, nombre: "Bulto X12 lamina rollo 14,5X14,5 x 500grs", tipo: "films", cantidad: 1, desc: "Calidad premium", precio: 700, talle: "unico", img: '../imagenes/laminapaty.png'},
+    {id: 11, nombre: "Bolsas X10 sobre Giganton 80X110", tipo: "CONSORCIO", cantidad: 1,desc: "Calidad premium", precio: 400, talle: "grande", img: '../imagenes/BOLSACONSORCIOSCRIPT.jpg'},
+    
+]
